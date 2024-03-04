926a5cdde793ac3ad300f325f40e4b6466caadd4
Bartosz Ratajczyk <bartosz.ratajczyk@codeclusive.io>
2024-03-04 13:44:43 +0100

    [Zadanie 1.3] Transform OX game to Object-Oriented solution.

--- zaj1/ox.py
+++ zaj1/ox.py
@@ -1,76 +1,100 @@
-ALL_SPACES = list('123456789')  # Klucze słownika planszy KIK.
-X, O, BLANK = 'X', 'O', ' '  # Stałe reprezentujące wartości tekstowe.
+class Board:
+    ALL_SPACES = list('123456789')  # Klucze słownika planszy KIK.
+    X, O, BLANK = 'X', 'O', ' '  # Stałe reprezentujące wartości tekstowe.
+
+    def __init__(self):
+        self.gameBoard = self.get_blank_board()
+
+    def get_blank_board(self):
+        board = {}
+        for space in self.ALL_SPACES:
+            board[space] = self.BLANK
+        return board
+
+    def get_board_str(self):
+        return f'''
+                {self.gameBoard['1']}|{self.gameBoard['2']}|{self.gameBoard['3']} 1 2 3 
+                -+-+- 
+                {self.gameBoard['4']}|{self.gameBoard['5']}|{self.gameBoard['6']} 4 5 6 
+                -+-+- 
+                {self.gameBoard['7']}|{self.gameBoard['8']}|{self.gameBoard['9']} 7 8 9'''
+
+    def is_valid_space(self, space):
+        if space is None:
+            return False
+        return space in self.ALL_SPACES and self.gameBoard[space] == self.BLANK
+
+    def is_winner(self, player):
+        b, p = self.gameBoard, player
+        return ((b['1'] == b['2'] == b['3'] == p) or  # poziomo na górze
+                (b['4'] == b['5'] == b['6'] == p) or  # poziomo w środku
+                (b['7'] == b['8'] == b['9'] == p) or  # poziomo u dołu
+                (b['1'] == b['4'] == b['7'] == p) or  # pionowo z lewej
+                (b['2'] == b['5'] == b['8'] == p) or  # pionowo w środku
+                (b['3'] == b['6'] == b['9'] == p) or  # pionowo z prawej
+                (b['3'] == b['5'] == b['7'] == p) or  # przekątna 1
+                (b['1'] == b['5'] == b['9'] == p))  # przekątna 2
+
+    def is_board_full(self):
+        for space in self.ALL_SPACES:
+            if self.gameBoard[space] == self.BLANK:
+                return False
+        return True
+
+    def update_board(self, space, mark):
+        self.gameBoard[space] = mark
 
 
-def main():
-    """Rozgrywka w kółko i krzyżyk."""
-    print('Witaj w grze kółko i krzyżyk!')
-    gameBoard = getBlankBoard()  # Utwórz słownik planszy KIK.
-    currentPlayer, nextPlayer = X, O  # X wykonuje ruch jako pierwszy, O jako następny.
-    while True:
-        print(getBoardStr(gameBoard))  # Wyświetl planszę na ekranie.
+class Game:
+    def __init__(self):
+        self.board = Board()
+        self.current_player = self.board.X
+        self.next_player = self.board.O
 
-        # Zadawaj graczowi pytanie, aż wprowadzi prawidłową liczbę od 1 do 9:
-        move = None
-        while not isValidSpace(gameBoard, move):
-            print(f'Jaki jest ruch gracza {currentPlayer}? (1-9)')
+    def main(self):
+        print('Witaj w grze kółko i krzyżyk!')
+
+        while True:
+            print(self.board.get_board_str())
+
+            move = None
+            move = self.make_move(move)
+            print(move)
+            self.board.update_board(move, self.current_player)
+
+            if self.check_winner():
+                break
+            elif self.check_board_full():
+                break
+
+            self.switch_players()
+
+        print('Dziękuję za grę!')
+
+    def switch_players(self):
+        self.current_player, self.next_player = self.next_player, self.current_player
+
+    def make_move(self, move):
+        while not self.board.is_valid_space(move):
+            print(f'Jaki jest ruch gracza {self.current_player}? (1-9)')
             move = input()
-        updateBoard(gameBoard, move, currentPlayer)  # Wykonanie ruchu.
-        # Sprawdzenie, czy gra jest zakończona:
-        if isWinner(gameBoard, currentPlayer):  # Sprawdzenie, kto wygrał.
-            print(getBoardStr(gameBoard))
-            print(currentPlayer + ' wygrał grę!')
-            break
-        elif isBoardFull(gameBoard):  # Sprawdzenie remisu.
-            print(getBoardStr(gameBoard))
-            print('Gra zakończyła się remisem!')
-            break
-        currentPlayer, nextPlayer = nextPlayer, currentPlayer  # Zmiana gracza.
-    print('Dziękuję za grę!')
+        return move
 
-
-def getBlankBoard():
-    """Tworzy nową, pustą planszę gry w kółko i krzyżyk."""
-    board = {}  # Plansza jest reprezentowana przez słownik Pythona.
-    for space in ALL_SPACES:
-        board[space] = BLANK  # Wszystkie pola na początku są puste.
-    return board
-
-
-def getBoardStr(board):
-    """Zwraca tekstową reprezentację planszy."""
-    return f'''
-            {board['1']}|{board['2']}|{board['3']} 1 2 3 
-            -+-+- 
-            {board['4']}|{board['5']}|{board['6']} 4 5 6 
-            -+-+- 
-            {board['7']}|{board['8']}|{board['9']} 7 8 9'''
-def isValidSpace(board, space):
-    """Zwraca True, jeśli pole na planszy ma prawidłowy numer i pole jest puste."""
-    if space is None:
+    def check_winner(self):
+        if self.board.is_winner(self.current_player):
+            print(self.board.get_board_str())
+            print(self.current_player + ' wygrał grę!')
+            return True
         return False
-    return space in ALL_SPACES or board[space] == BLANK
-def isWinner(board, player):
-    """Zwraca True, jeśli gracz jest zwycięzcą tej planszy KIK."""
-    b, p = board, player # Krótsze nazwy jako "składniowy cukier".
-    # Sprawdzenie, czy trzy takie same znaki występują w wierszach, kolumnach i po przekątnych.
-    return ((b['1'] == b['2'] == b['3'] == p) or # poziomo na górze
-            (b['4'] == b['5'] == b['6'] == p) or # poziomo w środku
-            (b['7'] == b['8'] == b['9'] == p) or # poziomo u dołu
-            (b['1'] == b['4'] == b['7'] == p) or # pionowo z lewej
-            (b['2'] == b['5'] == b['8'] == p) or # pionowo w środku
-            (b['3'] == b['6'] == b['9'] == p) or # pionowo z prawej
-            (b['3'] == b['5'] == b['7'] == p) or # przekątna 1
-            (b['1'] == b['5'] == b['9'] == p)) # przekątna 2
-def isBoardFull(board):
-    """Zwraca True, jeśli wszystkie pola na planszy są zajęte."""
-    for space in ALL_SPACES:
-        if board[space] == BLANK:
-            return False # Jeśli nawet jedno pole jest puste, zwracaj False.
-    return True # Nie ma wolnych pól, zatem zwróć True.
-def updateBoard(board, space, mark):
-    """Ustawia pole na planszy na podany znak."""
-    board[space] = mark
+
+    def check_board_full(self):
+        if self.board.is_board_full():
+            print(self.board.get_board_str())
+            print('Gra zakończyła się remisem!')
+            return True
+        return False
+
 
 if __name__ == '__main__':
-    main() # Wywołaj main(), jeśli ten moduł został uruchomiony, a nie zaimportowany.
+    game = Game()
+    game.main()
